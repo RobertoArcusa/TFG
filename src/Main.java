87c553edc9cf29f4a2df44e24be1e5590fa4ecc5
Roberto <web1201ral@gmail.com>
2025-05-16 22:02:36 +0200

    Commit prueba

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) {
         FlatLightLaf.setup();
 
-        // Llamamos a la clase LoginUsuario
+        // Llamamos a la clase LoginUsuario ..
+
         SwingUtilities.invokeLater(() -> {
             com.robertoarcusa.tfg.vistas.LoginUsuario ventana = new com.robertoarcusa.tfg.vistas.LoginUsuario();
             ventana.setVisible(true);
